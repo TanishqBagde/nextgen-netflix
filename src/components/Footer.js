8585915f107a87fd57ea26701d420abb0559c7af
TanishqBagde <tanishqbagde1618@gmail.com>
2024-06-02 17:31:00 +0530

    updates

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className = "bg-gray-400 shadow-md py-4 mt-8">
+            <div className ="container mx-auto text-center">
+                <p className ="text-gray-950">© 2024 AppleTv+Gpt. All rights are not reserved.</p>
+                
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -11,7 +11,7 @@ const MainContainer = () => {
     const randomIndex = Math.floor(Math.random() * movie.length);
     const { title, overview, id } = movie[randomIndex];
     return(
-        <div>
+        <div className ="p-4 md:p-8">
           <VideoTitle title={title} overview={overview}/>
           <VideoBackground movieId={id}/>
         </div>
